ec5f23d72d71b5309368231dac7560878e2ecf34
ReflexLevel0 <dominikdejanovic2@gmail.com>
2023-10-05 14:39:07 +0200

    Added comments

--- API/Query.cs
+++ API/Query.cs
@@ -11,11 +11,21 @@ public class Query
 		_db = db;
 	}
 
+	/// <summary>
+	/// Returns cpu logs
+	/// </summary>
+	/// <param name="serverId"></param>
+	/// <param name="startDateTime"></param>
+	/// <param name="endDateTime"></param>
+	/// <param name="interval">Interval that specifies time distance between two logs (for example, if interval is 10, then two logs of 5 minutes will be combined into a single log and returned)</param>
+	/// <param name="method">Method for combining multiple logs into one (min, max, avg, etc.)</param>
+	/// <returns></returns>
 	public async IAsyncEnumerable<Cpu> Cpu(int serverId, string? startDateTime, string? endDateTime, int interval, string method)
 	{
 		var cpuLogs = new List<Cpu>();
 		int intervalSum = 0;
 
+		//Combines multiple cpu logs into a single log
 		Cpu CombineCpuLogs()
 		{
 			double usageProcessed = QueryHelper.CombineValues(method, cpuLogs.Select(c => c.Usage));
@@ -25,8 +35,11 @@ public class Query
 
 		await foreach (var reader in _db.ExecuteReadAsync(
 			               $"SELECT serverid, date, interval, usage, numberoftasks " +
-			               $"FROM Cpu WHERE {QueryHelper.LimitSqlByParameters(serverId, startDateTime, endDateTime)} ORDER BY date;"))
+			               $"FROM Cpu " +
+			               $"WHERE {QueryHelper.LimitSqlByParameters(serverId, startDateTime, endDateTime)} " +
+			               $"ORDER BY date;"))
 		{
+			//Parsing db record data
 			int id = reader.GetInt32(0);
 			var date = reader.GetDateTime(1);
 			int logInterval = reader.GetInt32(2);
@@ -34,10 +47,12 @@ public class Query
 			int numberOfTasks = reader.GetInt32(4);
 			var cpuLog = new Cpu(id, date, logInterval, usage, numberOfTasks);
 			
+			//Adding cpu log to the list of logs
 			cpuLogs.Add(cpuLog);
 			intervalSum += cpuLog.Interval;
 			if (intervalSum < interval) continue;
 			
+			//Returning the combined log
 			yield return CombineCpuLogs();
 			cpuLogs.Clear();
 			intervalSum = 0;
