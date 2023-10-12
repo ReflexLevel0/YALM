ec23a5258afe40c89c78c7ca7e878bed4ab0daad
ReflexLevel0 <dominikdejanovic2@gmail.com>
2023-10-12 14:38:23 +0200

    Created base website

--- API/Program.cs
+++ API/Program.cs
@@ -5,7 +5,15 @@ builder.Services
 	.AddSingleton(new Db(File.ReadAllText("dbConnectionString.txt")))
 	.AddGraphQLServer()
 	.AddQueryType<Query>();
+builder.Services.AddCors(options =>
+{
+	options.AddPolicy("AllowAll", b =>
+	{
+		b.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+	});
+});
 
 var app = builder.Build();
+app.UseCors("AllowAll");
 app.MapGraphQL();
 await app.RunAsync();
