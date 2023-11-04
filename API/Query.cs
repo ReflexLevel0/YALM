bec4c76c95c16bb0886eade97ac574924aca7b9e
ReflexLevel0 <dominikdejanovic2@gmail.com>
2023-11-04 10:41:50 +0100

    Added ability to fetch memory from API

--- API/Query.cs
+++ API/Query.cs
@@ -19,7 +19,7 @@ public class Query
 	/// <param name="serverId"></param>
 	/// <param name="startDateTime"></param>
 	/// <param name="endDateTime"></param>
-	/// <param name="interval">Interval that specifies time distance between two logs (for example, if interval is 10, then two logs of 5 minutes will be combined into a single log and returned). If interval is null, then interval is decided dynamically (interval=1 minute for every hour between <param name="startDateTime"></param> and <param name="endDateTime"></param>).</param>
+	/// <param name="interval">Interval that specifies time distance between two logs. If interval is null, then interval is decided dynamically (interval=1 minute for every hour between <param name="startDateTime"></param> and <param name="endDateTime"></param>).</param>
 	/// <param name="method">Method for combining multiple logs into one (min, max, avg, etc.)</param>
 	/// <returns></returns>
 	public async IAsyncEnumerable<Cpu> Cpu(int serverId, string? startDateTime, string? endDateTime, int? interval, string? method)
@@ -39,7 +39,39 @@ public class Query
 		Func<NpgsqlDataReader, CpuLog> parseRecordFunc = reader => _db.ParseCpuRecord(reader);
 		var getEmptyRecordFunc = () => new Cpu(serverId, DateTime.Now, 0, 0);
 
-		await foreach (var log in QueryHelper.GetLogs(_db, "CPU", sqlSelectQuery, combineLogsFunc, parseRecordFunc, getEmptyRecordFunc, startDateTime, endDateTime, interval))
+		await foreach (var log in QueryHelper.GetLogs(_db, "Cpu", sqlSelectQuery, combineLogsFunc, parseRecordFunc, getEmptyRecordFunc, startDateTime, endDateTime, interval))
+		{
+			yield return log;
+		}
+	}
+
+	/// <summary>
+	/// Returns memory logs
+	/// </summary>
+	/// <param name="serverId"></param>
+	/// <param name="startDateTime"></param>
+	/// <param name="endDateTime"></param>
+	/// <param name="interval">Interval that specifies time distance between two logs. If interval is null, then interval is decided dynamically (interval=1 minute for every hour between <param name="startDateTime"></param> and <param name="endDateTime"></param>).</param>
+	/// <param name="method">Method for combining multiple logs into one (min, max, avg, etc.)</param>
+	/// <returns></returns>
+	public async IAsyncEnumerable<Memory> Memory(int serverId, string? startDateTime, string? endDateTime, int? interval, string? method)
+	{
+		string selectQuery = $"SELECT serverid, date, interval, mbused, mbtotal " +
+		                     $"FROM Memory " +
+		                     $"WHERE {QueryHelper.LimitSqlByParameters(serverId, startDateTime, endDateTime)} " +
+		                     $"ORDER BY date";
+
+		Func<IList<MemoryLog>, Memory> combineLogsFunc = logs =>
+		{
+			int mbused = (int)QueryHelper.CombineValues(method, logs.Select(l => (double)l.MbUsed));
+			int mbtotal = (int)QueryHelper.CombineValues(method, logs.Select(l => (double)l.MbTotal));
+			return new Memory(serverId, logs.First().Date, mbused, mbtotal);
+		};
+		
+		Func<NpgsqlDataReader, MemoryLog> parseRecordFunc = reader => _db.ParseMemoryRecord(reader);
+		var getEmptyRecordFunc = () => new Memory(serverId, DateTime.Now, 0, 0);
+		
+		await foreach(var log in QueryHelper.GetLogs(_db, "Memory", selectQuery, combineLogsFunc, parseRecordFunc, getEmptyRecordFunc, startDateTime, endDateTime, interval))
 		{
 			yield return log;
 		}
